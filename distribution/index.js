d92007fa86a07dbf8f3e23325a7620b3e4952f44
Cristiano Betta <cbetta@gmail.com>
2016-04-29 14:21:32 +0100

    Add Commander
    * Add Commander library for parsing CLI arguments

--- distribution/index.js
+++ distribution/index.js
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
 'use strict';
 
-console.log('Hello, world?');
+var _commander = require('commander');
+
+var _commander2 = _interopRequireDefault(_commander);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_commander2.default.version('0.0.1').parse(process.argv);
+
+console.log("Hello World");
--- source/index.js
+++ source/index.js
@@ -1,2 +1,10 @@
 #!/usr/bin/env node
-console.log('Hello, world');
+
+import commander from 'commander';
+
+commander
+  .version('0.0.1')
+  .parse(process.argv);
+
+
+console.log("Hello World");
